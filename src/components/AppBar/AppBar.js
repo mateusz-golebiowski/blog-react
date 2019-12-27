0021c2e36bff4031c86c9d4b4ab823f3aec6959d
Mateusz Gołębiowski <golebiowski.mateusz@outlook.com>
2019-12-27 11:03:31 +0100

    update appbar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import {NavLink} from "react-router-dom";
@@ -23,17 +27,16 @@ const useStyles = makeStyles(theme => ({
 export default function BlogAppBar(props) {
     const classes = useStyles();
     const handleSignOut = () => {
-      signOut();
-      props.setUserToken('');
+        handleCloseMenu();
+        signOut();
+        props.setUserToken('');
     };
+    const [anchorEl, setAnchorEl] = useState(null);
+
+
     const loginButton = () => {
         if (props.userToken !== '') {
-            return (
-                <Button onClick={handleSignOut} color="secondary" className={classes.button}>
-                    Wyloguj się
-                </Button>
-
-            );
+            return null;
         } else {
             return (
                 <NavLink to="/signIn" activeClassName="active">
@@ -59,11 +62,29 @@ export default function BlogAppBar(props) {
             return null;
         }
     };
+    const handleClickMenu = event => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleCloseMenu = () => {
+        setAnchorEl(null);
+    };
+    const showMenuButton = () => {
+        if(props.userToken !== ''){
+            return (
+                <IconButton edge="start" className={classes.menuButton} onClick={handleClickMenu} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+            )
+        } else {
+            return null;
+        }
+    };
 
     return (
         <div className={classes.root}>
             <AppBar position="fixed" >
                     <Toolbar>
+                        {showMenuButton()}
                         <NavLink to="/" exact activeClassName="active">
                             <Button color="secondary" className={classes.button}>
                                 Strona główna
@@ -71,12 +92,19 @@ export default function BlogAppBar(props) {
                         </NavLink>
                         {loginButton()}
                         {addNewPostButton()}
-                            <Button color="secondary" className={classes.button}>
-                                Kontakt
-                            </Button>
-
                     </Toolbar>
                 </AppBar>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleCloseMenu}
+                >
+                    <MenuItem onClick={handleCloseMenu}>Mój profil</MenuItem>
+                    <MenuItem onClick={handleCloseMenu}>Użytkownicy</MenuItem>
+                    <MenuItem onClick={handleSignOut}>Wyloguj się</MenuItem>
+                </Menu>
             <Toolbar></Toolbar>
         </div>
     );
