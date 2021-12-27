044311dd2171f2813572dd0f01ff7dd3b3e259b3
Mateusz Gołębiowski <golebiowski.mateusz@outlook.com>
2021-12-27 12:32:08 +0100

    downloading users

---
+++ src/lib/config.js
@@ -0,0 +1,13 @@
+import {getUserToken} from "./user";
+
+export const apiUrl = `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}`;
+export const fetcher = (url) => {
+    console.log(url)
+    return fetch(url,{
+        method: 'get',
+        headers: {
+            'Accept': 'application/json',
+            'authorization': getUserToken()
+        }
+    }).then(res => res.json())
+}
