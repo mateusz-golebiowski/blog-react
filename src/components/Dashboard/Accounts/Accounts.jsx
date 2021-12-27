b7e6d60390bb744dbcc381b4dd634625527822f9
Mateusz Gołębiowski <golebiowski.mateusz@outlook.com>
2021-12-27 12:32:08 +0100

    downloading users

--- src/components/Dashboard/Accounts/Accounts.jsx
+++ src/components/Dashboard/Accounts/Accounts.jsx
@@ -16,7 +16,21 @@ import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import { useIntl, FormattedMessage } from 'react-intl';
 import { useHistory } from "react-router-dom";
+import {apiUrl, fetcher} from "../../../lib/config";
+import useSWR from 'swr'
+import {getUserToken} from "../../../lib/user";
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+
 const drawerWidth = 240;
+
 
 const useStyles = makeStyles((theme) => ({
 
@@ -26,23 +40,90 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-
+const columns = [
+    { id: 'firstName', label: 'Name', minWidth: 170 },
+    { id: 'lastName', label: 'last name', minWidth: 100 },
+    {
+        id: 'email',
+        label: 'email',
+    },
+    {
+        id: 'role',
+        label: 'role',
+        format: (value) => value.name
+    }
+];
 
 export default function Accounts(props) {
     const classes = useStyles();
     const intl = useIntl();
+    const { data, error } = useSWR(`${apiUrl}/api/v1/user/getAllData`, fetcher)
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
 
     return (
         <>
             <main className={classes.content}>
                 <Toolbar />
                 <Typography paragraph>
-                   Accounts
+                    {data && (<Paper>
+                    <TableContainer sx={{ maxHeight: 440 }}>
+                        <Table stickyHeader aria-label="sticky table">
+                            <TableHead>
+                                <TableRow>
+                                    {columns.map((column) => (
+                                        <TableCell
+                                            key={column.id}
+                                            style={{ minWidth: column.minWidth }}
+                                        >
+                                            {column.label}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {data
+                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    .map((row) => {
+                                        return (
+                                            <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                                                {columns.map((column) => {
+                                                    console.log(row)
+                                                    const value = row[column.id];
+                                                    return (
+                                                        <TableCell key={column.id} align={column.align}>
+                                                            {column.format
+                                                                ? column.format(value)
+                                                                : value}
+                                                        </TableCell>
+                                                    );
+                                                })}
+                                            </TableRow>
+                                        );
+                                    })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TablePagination
+                        rowsPerPageOptions={[10, 25, 100]}
+                        component="div"
+                        count={data.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    /></Paper>)}
                 </Typography>
-                <Typography paragraph>
-                  Accountsyy
-                </Typography>
+
             </main>
         </>
     );
